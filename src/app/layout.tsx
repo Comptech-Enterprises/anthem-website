5a2825e52920c9aedc9647ff443d2f13762944c2
dnegi-comptech <dnegicomptech@gmail.com>
2026-09-23 11:53:21 +0530

    chore(seo): add google-site-verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export const metadata: Metadata = {
     icon: "/logo.webp",
     apple: "/logo.webp",
   },
+  verification: {
+    google: "9oULDvmqmUDX2qp5DFvHGseyDvjdJNLbeOcQkwc9-Ks",
+  },
 };
 
 export default function RootLayout({
